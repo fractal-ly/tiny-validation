deb9c128e6f0a03393d9d839d746ef76032abb84
Gustavo Gallegos <meaning.app>
2021-07-20 16:50:15 -0700

    Add test for email validator

--- src/lib/validation.spec.ts
+++ src/lib/validation.spec.ts
@@ -1,11 +1,19 @@
 import test from 'ava';
 
 import { Schema, validate } from './validation';
-import { isPresent, isTrue, maxChars, minChars, minVal } from './validators';
+import {
+  isEmail,
+  isPresent,
+  isTrue,
+  maxChars,
+  minChars,
+  minVal,
+} from './validators';
 
 const toValidate = {
   name: 'Filiberto Umberticula',
   password: '2309udifhasdkfhlkweru',
+  email: 'something@somewhere',
   tos: true,
   age: 23,
 };
@@ -15,6 +23,7 @@ const schema: Schema = {
   password: { initial: '', validations: [isPresent, minChars(8)] },
   tos: { initial: false, validations: [isTrue] },
   age: { initial: 0, validations: [minVal(18)] },
+  email: { initial: '', validations: [isPresent, isEmail] },
 };
 
 test('Test successful validation', (t) => {
@@ -35,6 +44,7 @@ test('Test failing validation', (t) => {
   t.is(result.isFail, true);
   t.deepEqual(result.x, {
     age: ['age has to be greater than 18'],
+    email: ['email is not present', 'email must be a string'],
     name: ['name is not present', 'name has to be greater than 3 chars'],
     password: ['password has to be greater than 8 chars'],
     tos: ['tos must be set'],
